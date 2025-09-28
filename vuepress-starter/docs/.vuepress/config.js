ff5d4a92ef401b9203f276ee35380bc635c66125
Blndm01 <blndbargrkaye@gmail.com>
2025-09-28 22:31:24 +0300

    update source

--- vuepress-starter/docs/.vuepress/config.js
+++ vuepress-starter/docs/.vuepress/config.js
@@ -5,6 +5,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
+  base: '/blnd-ismael-mustafa/',
 
   title: 'VuePress',
   description: 'My first VuePress Site',
